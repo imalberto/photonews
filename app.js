fbdc1e4f0fdf83d4c0e802c8376f92500d029679
imalberto <imalberto@gmail.com>
2013-11-04 14:50:58 -0800

    Updated.

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var debug = require('debug')('app'),
     expstate = require('express-state'),
     expview = require('express-view'),
     expyui = require('express-yui'),
+    router = require('lib/router'),
     Locator = require('locator'),
     LocatorHandlebars = require('locator-handlebars'),
     LocatorYUI = require('locator-yui'),
@@ -32,11 +33,13 @@ global.PN = {
 };
 
 ////
-// app setup
+// setup Locator
 loc = new Locator({
     buildDirectory: __dirname + '/build'
 });
 
+////
+// app setup
 app = express();
 
 appPort = process.env.PORT || 8666;
@@ -68,57 +71,57 @@ loc.plug(new LocatorHandlebars({ format: 'yui' }))
     // Add more locator-plugins here as necessary
     .parseBundle(__dirname);
 
+
 app.yui.ready(function (err) {
     if (err) {
-        console.log(err);
-        console.log(err.stack);
+        console.error('------------------------------------------------------');
+        console.error(err);
+        console.error(err.stack);
+        console.error('------------------------------------------------------');
         return;
     }
 
    // TODO helper to load all those modules detected by Locator
+   // app.yui.use(Object.keys(app.yui._serverModules))
     var Y = app.yui.use('util',
                         'default-controller', 'default-model',
                         'news-controller', 'photos-controller',
                         'news-model', 'photos-model',
                         'post-model', 'photo-model',
-                        'news-handler'),
-        router = require('lib/router');
+                        'news-handler');
 
-    Y.Env.runtime = 'server';
+
     router.extend(app);
 
-    // TODO find a more efficient way to determine if all modules are loaded
-    if (!Y.Models.DefaultModel) {
-        console.error('** ERROR **: YUI modules not loaded in server instance');
-        return;
-    }
+    // Showing the various usecases to register routes
+    app.page('home', '/'); // setting '/' as the path vs the default '/home'
+    app.page('news', '/news'); // same as app.page('news');
+    app.page('photos'); // will default to '/photos'
 
-    // Showing the various ways to register routes
-    app.page('home', '/'); // use default model, default controller
-    app.page('news', '/news'); // use custom model, default controller
-    app.page('photos'); // use custom model, custom controller
+    // app.page('about', '/about');
 
-    // app.page('about', '/about'); // only static content; default model, default controller
-    /*
-    app.page('contact', '/aboutus', function (req, res) {
-        res.render('aboutus');
-    });
-    app.page('admin', '/admin', function (req, res, next) {
-        // authenticate request here
-        req.params.isAuth = true;
-        next();
-    }, function (req, res) {
-        if (req.params.isAuth) {
-            res.render('admin');
-        } else {
-            res.render('notfound');
-        }
-    });
-    */
+    //
+    // app.page('contact', '/aboutus', function (req, res) {
+    //     res.render('aboutus');
+    // });
+
+    // add a middleware, and a handler
+    // app.page('admin', '/admin', function (req, res, next) {
+    //     // authenticate request here
+    //     req.params.isAuth = true;
+    //     next();
+    // }, function (req, res) {
+    //     if (req.params.isAuth) {
+    //         res.render('admin');
+    //     } else {
+    //         res.render('notfound');
+    //     }
+    // });
 
     PN.ROUTES.routes = app.getRouteMap();
     PN.VIEWS.views = app.getViewsConfig();
     app.expose(PN, 'PN');
+    // app.use(router.expose());
 
     app.listen(appPort, function () {
         console.log('Ready to serve on port %s', appPort);
