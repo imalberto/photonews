bbd9c74919580fbafd5d89c7b24b708ae6a9ba76
imalberto <imalberto@gmail.com>
2013-11-15 17:22:20 -0800

    Linted

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,14 +21,21 @@ module.exports = function (grunt) {
                 files: [{
                     expand: true,
                     cwd: 'public/js/',
-                    src: ['pnapp.js', 'controllers/*.js', 'handlers/*.js', 'models/*.js', 'views/*.js'],
+                    src: [
+                        'pnapp.js', 'controllers/*.js', 'handlers/*.js',
+                        'models/*.js', 'views/*.js'
+                    ],
                     dest: 'tmp/'
                 }]
             }
         },
         watch: {
             scripts: {
-                files: ['public/js/pnapp.js', 'public/js/controllers/*.js', 'public/js/handlers/*.js', 'public/js/models/*.js', 'public/js/views/*.js'],
+                files: [
+                    'public/js/pnapp.js', 'public/js/controllers/*.js',
+                    'public/js/handlers/*.js', 'public/js/models/*.js',
+                    'public/js/views/*.js'
+                ],
                 tasks: ['transpile'],
                 options: {
                     spawn: false
